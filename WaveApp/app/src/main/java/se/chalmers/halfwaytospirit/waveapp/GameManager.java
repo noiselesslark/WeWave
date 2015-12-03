e0b69557f3d2a54cf699443b6e7781785b03fc98
noiselesslark <elisekarlsson@gmail.com>
2015-12-03 14:35:03 +0100

    Add base classes.

---
+++ WaveApp/app/src/main/java/se/chalmers/halfwaytospirit/waveapp/GameManager.java
@@ -0,0 +1,10 @@
+package se.chalmers.halfwaytospirit.waveapp;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Elise on 03/12/2015.
+ */
+public class GameManager {
+    private ArrayList<Player> players = new ArrayList<Player>();
+}
