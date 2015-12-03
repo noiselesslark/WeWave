34c0eef41d6c8b4a487174436c322b88cbaa3625
lordchaterton <aya_la_kogal@hotmail.com>
2015-12-03 16:27:56 +0100

    Start doing the first and last screens layout

--- WaveApp/app/src/main/java/se/chalmers/halfwaytospirit/waveapp/EndGameActivity.java
+++ WaveApp/app/src/main/java/se/chalmers/halfwaytospirit/waveapp/EndGameActivity.java
@@ -1,7 +1,11 @@
 package se.chalmers.halfwaytospirit.waveapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class EndGameActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class EndGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_game);
+
+        ImageButton playAgainButton = (ImageButton) findViewById(R.id.lastScreenPlayAgainButton);
+        playAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainGameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- WaveApp/app/src/main/java/se/chalmers/halfwaytospirit/waveapp/StartScreenActivity.java
+++ WaveApp/app/src/main/java/se/chalmers/halfwaytospirit/waveapp/StartScreenActivity.java
@@ -1,7 +1,10 @@
 package se.chalmers.halfwaytospirit.waveapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartScreenActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class StartScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        Button playButton = (Button) findViewById(R.id.firstScreenPlayButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+
+                Intent intent = new Intent(view.getContext(), MainGameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
