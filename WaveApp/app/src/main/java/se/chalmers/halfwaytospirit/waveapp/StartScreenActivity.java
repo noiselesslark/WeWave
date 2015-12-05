be13d5df85b2f263d29b4fc80cdc518c1b2dac8d
noiselesslark <elisekarlsson@gmail.com>
2015-12-05 13:25:41 +0100

    Remove title bar from main game

--- WaveApp/app/src/main/java/se/chalmers/halfwaytospirit/waveapp/StartScreenActivity.java
+++ WaveApp/app/src/main/java/se/chalmers/halfwaytospirit/waveapp/StartScreenActivity.java
@@ -2,17 +2,12 @@ package se.chalmers.halfwaytospirit.waveapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
 
 public class StartScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //Remove title bar
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         setContentView(R.layout.activity_start_screen);
     }
 }
